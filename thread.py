9a8158aba6e7ee2151cc706058d5576499de7a10
hooah26 <hooah26@sweetk.co.kr>
2023-07-31 13:55:26 +0900

    test

--- thread.py
+++ thread.py
@@ -43,11 +43,11 @@ def rgb_camera(camera_index, window_name):
 
 
 def depth_camera():
-    # Configure depth and color streams
+    # Configure color and depth streams
     pipeline = rs.pipeline()
     config = rs.config()
-    config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
-    config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
     # Start streaming
     pipeline.start(config)
@@ -95,7 +95,7 @@ thread1.start()
 thread2.start()
 
 # Wait for the threads to finish
-thread1.join()
-thread2.join()
+# thread1.join()
+# thread2.join()
 
 cv2.destroyAllWindows()
