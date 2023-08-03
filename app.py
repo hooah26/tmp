f8166a8ae177288c3df9e6a5e058bc514b6845ac
Jaewook_Kim <96579508+hooah26@users.noreply.github.com>
2023-08-03 10:17:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sys
-import cv2 
+import cv2
 import imutils
 from yoloDet import YoloTRT
 
@@ -18,11 +18,16 @@ while True:
     fps = 1 / t
     fps_text = "FPS: {:.2f}".format(fps)
 
+    # Iterate over the detections and only consider those with class ID for people (typically 0 for COCO)
+    for obj in detections:
+        if obj['class'] == 0: # assuming class ID for 'person' is 0
+            box = obj['box']
+            x, y, w, h = map(int, box)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # Add more code here if you want to do something specific with the detected people
+
     # Display FPS on the frame
     cv2.putText(frame, fps_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-    # for obj in detections:
-    #    print(obj['class'], obj['conf'], obj['box'])
 
     cv2.imshow("Output", frame)
     key = cv2.waitKey(1)
