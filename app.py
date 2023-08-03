bd5e1037073eb816231a609db20d38f382eecf71
Jaewook_Kim <96579508+hooah26@users.noreply.github.com>
2023-08-03 13:05:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import imutils
 from yoloDet import YoloTRT
 
 # use path for library and engine file
-model = YoloTRT(library="yolov5/build/libmyplugins.so", engine="yolov5/build/yolov5s.engine", conf=0.5, yolo_ver="v5")
+model = YoloTRT(library="yolov7/build/libmyplugins.so", engine="yolov7/build/yolov7-tiny.engine", conf=0.5, yolo_ver="v7")
 
 # Use the default camera connected to the system
 cap = cv2.VideoCapture(0)
@@ -18,16 +18,11 @@ while True:
     fps = 1 / t
     fps_text = "FPS: {:.2f}".format(fps)
 
-    # Iterate over the detections and only consider those with class ID for people (typically 0 for COCO)
-    for obj in detections:
-        if obj['class'] == 0: # assuming class ID for 'person' is 0
-            box = obj['box']
-            x, y, w, h = map(int, box)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            # Add more code here if you want to do something specific with the detected people
-
     # Display FPS on the frame
     cv2.putText(frame, fps_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    # for obj in detections:
+    #    print(obj['class'], obj['conf'], obj['box'])
 
     cv2.imshow("Output", frame)
     key = cv2.waitKey(1)
@@ -36,3 +31,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+~                           
