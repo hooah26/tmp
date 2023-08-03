87d7869e21f16af8a39a8a6b827f1cf0b5e19a16
Jaewook_Kim <96579508+hooah26@users.noreply.github.com>
2023-08-03 13:08:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ while True:
 
     # Filter and draw only human detections
     for obj in detections:
+        print(ojb)
         if obj['class'] == 'person': # Assuming the human class is labeled as 'person'
             conf = obj['conf']
             box = obj['box']
